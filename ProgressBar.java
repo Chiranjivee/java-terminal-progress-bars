590ceb6824f903dd1c6e01df2273490cbdc2b226
CJ <cjthehacker7@gmail.com>
2018-10-11 01:08:53 +0530

    First commit add progress bar

---
+++ ProgressBar.java
@@ -0,0 +1,44 @@
+import java.io.File;
+
+class ProgressBar
+{
+    public static void main(String[] args) {
+        File file = new File("D:\\WorkDir\\CloudBuild\\src\\Android\\mobile\\DemoProj_348.apk");
+        file.mkdirs();
+    }
+
+   public static void main2(String[] args) throws InterruptedException {
+      
+        String progressBar = "[          ]";
+        int countDown = 10;
+        try
+        {
+            while (countDown >= 0)
+            {
+                System.out.print(progressBar);   
+                if (countDown == 0) break;
+                deleteBar();
+                progressBar = updateProgressBar(progressBar, countDown);   
+                countDown--;
+            }
+        }
+        catch (InterruptedException ex)
+        {
+        }
+    }
+
+    public static String updateProgressBar(String progressBar, int count) {
+        int equalLoc = 10 - count + 1;
+        StringBuilder myName = new StringBuilder(progressBar);
+        myName.setCharAt(equalLoc, '=');
+        return myName.toString();
+    }
+
+    public static void deleteBar() throws InterruptedException {
+        for (int i = 0; i < 12; i++) {
+            System.out.print("\b");
+        }
+
+        Thread.sleep(100);
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Java Terminal Progress bars
+
+This repository stores examples of progress bars created for terminal
+
+Compile:
+    
+    javac ProgressBar.java
+
+Run:
+
+    java ProgressBar
